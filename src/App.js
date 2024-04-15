a4abde51638317fb66efa1d85e79df06de21a3bd
searoTyp <nahiyanm@bu.edu>
2024-04-14 20:37:11 -0400

    I added gorcery list and made edits to app.js page so that we can use react router-dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 /****THIS WILL NOT WORK WITHOUT RUNNING THE FOLLOWING LINE****
 npm install react-router-dom axios*/
+// App.js
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import MainPage from './MainPage';
+import GroceryList from './GroceryList';
 
 function App() {
   return (
-    <div className="App">
-      <MainPage />
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={MainPage} />
+        <Route path="/grocery-list" component={GroceryList} />
+        {/* Add more routes as needed */}
+      </Switch>
+    </Router>
   );
 }
 
