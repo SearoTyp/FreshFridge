51aacede4724adf6ef590058e4e2d8dfa8221ba1
aaronvq <aaronvq@bu.edu>
2024-04-14 21:19:06 -0400

    aaron's test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 /****THIS WILL NOT WORK WITHOUT RUNNING THE FOLLOWING LINE****
 npm install react-router-dom axios*/
 import React from 'react';
-import MainPage from './MainPage';
+import MainPage from './MainPage.js';
 
 function App() {
   return (
