31e703977789c68708e94abb0c6c74e050c8f07f
searoTyp <nahiyanm@bu.edu>
2024-04-14 21:01:43 -0400

    Reverted back the errors troubleshooting the grocery list file right niw

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 /****THIS WILL NOT WORK WITHOUT RUNNING THE FOLLOWING LINE****
 npm install react-router-dom axios*/
-// App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import MainPage from './MainPage';
-import GroceryList from './GroceryList';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/" exact component={MainPage} />
-        <Route path="/grocery-list" component={GroceryList} />
-        {/* Add more routes as needed */}
-      </Switch>
-    </Router>
+    <div className="App">
+      <MainPage />
+    </div>
   );
 }
 
