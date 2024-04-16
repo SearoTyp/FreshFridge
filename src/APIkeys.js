b5351132e0feaa67d10bf56891e7593a5f5b1e6e
Alex M <munteana@bu.edu>
2024-04-16 16:51:03 -0400

    API Keys

---
+++ src/APIkeys.js
@@ -0,0 +1,8 @@
+/*Recipe Search API:
+Application ID: 4bac8aa9
+Application Keys: 5de18e0d04cd5dd3685c82bb2aff5bad	—
+
+Food Database API:
+Application ID: 36cbd87f
+Application Keys: 33696a1b421fe2be0f147a0b16daef78	—
+*/
