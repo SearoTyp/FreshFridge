89c7aa5a1c85e012cf2bf7e7f989cf1480bc408e
aaronvq <aaronvq@bu.edu>
2024-04-20 17:06:06 -0400

    added home page router dont know why things turned black

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
 import GroceryList from './GroceryList';
 import Recipes from './Recipes';
+import Homepage from './Homepage';
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
         <Route path="/" element={<MainPage />} />
         <Route path="/grocery-list" element={<GroceryList />} />
         <Route path="/recipes" element={<Recipes />} />
+        <Route path="/homepage" element={<Homepage />} />
       </Routes>
     </BrowserRouter>
   );
